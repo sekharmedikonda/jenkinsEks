5e8f68f2ba8744c519c34167e5d9483d3160a2ac
sekharmedikonda <149560593+sekharmedikonda@users.noreply.github.com>
2023-11-14 11:54:28 +0530

    Delete EKS directory

--- EKS/data.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3f6451bc97c18c3b0875f2727b5a273019b760e1ca3bf3a58224f8065c5a9b5a
-size 40
--- EKS/provider.tf
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:949def781d825a28ba12c7dd1dc6fcbb7e8d9272713043d333d3d9f9e7aff6e6
-size 47
